5bdb26a8c0c14f39b5841833603de099d9673a7a
Rosheen <rosheenmail@gmail.com>
2015-12-20 20:28:02 -0500

    initial commit

---
+++ DataStructures/LinkedList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    class LinkedList
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+
+        public LinkedList(Node head)
+        {
+            this.Head = head; 
+        }
+
+        /// <summary>
+        /// Inserts a node to the end of a LinkedList
+        /// </summary>
+        /// <param name="nodeToInsert"></param>
+        /// <param name="currList"></param>
+        /// <returns></returns>
+        public LinkedList Insert(Node nodeToInsert, LinkedList currList)
+        {
+            if (nodeToInsert == null) //inserting null node
+            {
+                Console.WriteLine("Cannot insert Null node");
+                return currList;
+            }
+
+            if (currList.Head == null)//if list is empty
+            {
+                currList.Head = nodeToInsert;
+                currList.Tail = nodeToInsert;
+            }
+            else//appending to the end of list
+            {
+                currList.Tail.Next = nodeToInsert;
+                currList.Tail = currList.Tail.Next; 
+            }
+
+            return currList; 
+        }/*end of insert method*/
+
+        public void PrintList(LinkedList currList)
+        {
+            Node ptr = currList.Head;
+
+            while (ptr != null)
+            {
+                Console.Write(ptr.Data + " ");
+                ptr = ptr.Next;
+            }
+
+        }/*end of printlist method*/
+    }
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make new linkedlist and test all methods here
+            LinkedList currList = new LinkedList(null);
+            Node tmp = new Node(5, null);
+            currList = currList.Insert(tmp, currList);
+            Node tmp2 = new Node(7, null);
+            currList = currList.Insert(tmp2, currList);
+            Node tmp3 = new Node(9, null);
+            currList = currList.Insert(tmp3, currList);
+
+            currList.PrintList(currList);
+        }
+    }
+}
